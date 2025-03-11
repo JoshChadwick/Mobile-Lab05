b8ca276d2b501955ad9800765ceffbfc35033d78
JoshChadwick <22483403@stu.mmu.ac.uk>
2025-03-11 17:48:26 +0000

    Beginning Monkey Service

--- Part 2 - MVVM/MonkeyFinder/Services/MonkeyService.cs
+++ Part 2 - MVVM/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,20 @@
 ﻿namespace MonkeyFinder.Services;
-
+using System.Net.Http.Json;
 public class MonkeyService
 {
+    List<Monkey> monkeyList = new();
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        return monkeyList;
+    }
+
+
 }
